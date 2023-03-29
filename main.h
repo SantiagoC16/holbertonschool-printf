fd67c8a5137626ba069d795c5bf3a463fc519f0a
Santiago <6317@holbertonstudents.com>
2023-03-29 06:54:27 -0700

    prototypes

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef printf
+#define printf
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * struct printf - struct
+ *
+ * 
+ *
+ */
+
+typedef struct print
+{
+	;
+	;
+} print;
+
+int _printf(const char *format, ...);
+
+
+#endif
