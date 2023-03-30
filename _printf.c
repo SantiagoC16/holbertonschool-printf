c456e03dc4aefe225509507bab23551c5fda0136
Santiago <6317@holbertonstudents.com>
2023-03-29 20:13:31 -0700

    ex 0

--- _printf.c
+++ _printf.c
@@ -12,13 +12,37 @@ int _printf(const char *format, ...)
 {
 va_list _print;
 int cont;
-
+int cont2 = 0;
+char ch;
+char *str;
 
 va_start(_print, format);
 for (cont = 0; format[cont] != '\0'; cont++)
 {
-
-
+	if (format[cont] == '%')
+	{
+		cont2++
+		{
+			if (format[cont2] = 's')
+			{
+				char *str = va_arg(_print, char *);
+				_putchar(xD);
+			}
+			if (format[cont2] = 'c')
+			{
+				char ch = va_arg(_print, int);
+				_putchar(xD);
+			}
+			if (format[cont2] = '%')
+			{
+				_putchar('%');
+			}
+		}
+	}
+	else
+	{
+		xD
+	}
 }
 
 
