7e21d31207211690d0176c3d688afd238668c5c1
miguel damasco <migueldamasco111@gmail.com>
2023-03-31 23:22:25 -0700

    most of the chekers should works

--- _printf.c
+++ _printf.c
@@ -9,66 +9,74 @@
  *
  * Return: 0
  */
-int _printf(const char *format, ...)
-{
-  va_list _print;
-  int cont;
-  int i;
-  int cont2 = 0;
-  int skip = 0;
-  char ch;
-  char *str;
-
-  va_start(_print, format);
-  for (cont = 0; format[cont] != '\0'; cont++)
-    {
-      cont2++;
-      if (format[cont] == '%')
-	{
-	  if (format[cont+1] == 's')
-	    {
-	      cont2 -= 2;
-	      char *str = va_arg(_print, char *);
-	      int size = strlen(str);
-	      for (i = 0; i < size; i++)
-		{
-		  cont2++;
-		  putchar(str[i]);
-		}
-	    }
-	  if (format[cont+1] == 'c')
-	    {
-	      cont2--;
-	      char ch = va_arg(_print, int);
-	      putchar(ch);
-	    }
-	  if (format[cont+1] != 's' && format[cont+1] != 'c')
-	    {
-	      return (-1);
-	    }
-	  skip = 1;
-	}
-      else if (skip != 1)
-	{
-	  putchar(format[cont]);
-	}
-      else if (skip == 1)
-	{
-	  skip = 0;
-	  continue;
-	}
-    }
-  return (cont2);
-  va_end(_print);
-}
-
-int _strlen(char *str)
+int print_null(void)
 {
   int i, count = 0;
-  for(i = 0; str[i] != '\0'; i++)
+  char nothing[] = "(null)";
+  for (i = 0; i < 6; i++)
     {
+      putchar(nothing[i]);
       count++;
     }
-  return count;
+  return (count);
 }
+
+int _printf(const char *format, ...)
+{
+  int _printf(const char *format, ...)
+  {
+    va_list _print;
+    int cont, i, cont2 = 0, skip = 0, size, amount = 0;
+    char *str = NULL;
+    va_start(_print, format);
+    for (cont = 0; format[cont] != '\0'; cont++)
+      {
+	cont2++;
+	if (format[cont] == '%' && amount == 0)
+	  {
+	    switch (format[cont + 1])
+	      {
+	      case 's':
+		cont2 -= 2;
+		str = va_arg(_print, char *);
+		if (str != NULL)
+		  {
+		    size = strlen(str);
+		    for (i = 0; i < size; i++)
+		      {
+			cont2++;
+			putchar(str[i]);
+		      }
+		  }
+		else
+		  cont2 += print_null();
+		skip = 1;
+		break;
+	      case 'c':
+		putchar(va_arg(_print, int));
+		skip = 1;
+		cont2--;
+		break;
+	      case '%':
+		amount++;
+		cont2--;
+		putchar('%');
+		skip = 1;
+		break;
+	      case ' ':
+		return (-1);
+	      }
+	  }
+	else if (skip != 1)
+	  {
+	    putchar(format[cont]);
+	    if (format[cont + 1] == '%')
+	      amount = 0;
+	  }
+	else if (skip == 1)
+	  skip = 0;
+      }
+    return (cont2);
+    va_end(_print);
+ }
 
