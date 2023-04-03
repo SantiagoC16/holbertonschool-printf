5d4ff89d39a7a52955d19feb616ea22331775601
miguel damasco <migueldamasco111@gmail.com>
2023-04-02 19:47:08 -0700

    documentation

--- more_aux_functions.c
+++ more_aux_functions.c
@@ -12,26 +12,26 @@ int count = 0, i, digit;
 int number = va_arg(_print, int);
 char print_char;
 
-if (number < 0)
+if (number < 0) /*if number is negative print char - number sign change */
 {
 	write(1, "-", 1);
 	count++;
 	number = -number; }
 
-if (number == 0)
+if (number == 0) /* if number is 0 print 0 */
 {
 	write(1, "0", 1);
 	count++; }
-else
+else /* if number is not 0 */
 {
 	int num_digits = 0;
 	int position = number;
 
-	while (position != 0)
+	while (position != 0)/* count the amount of digits dividing by 10 number to reach 0 increasing num_digits per iteration */
 	{
 		num_digits++;
 		position /= 10; }
-		while (num_digits > 0)
+		while (num_digits > 0) /* extract each digit of number, convert the digit into a char and print it with te function write, count increase 1 per digit */
 		{
 			int divisor = 1;
 
