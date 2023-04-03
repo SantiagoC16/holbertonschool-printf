d51792cd3738e33443955d3a1501328f35a96c60
miguel damasco <migueldamasco111@gmail.com>
2023-04-02 19:30:25 -0700

    documentation

--- print_all.c
+++ print_all.c
@@ -17,15 +17,15 @@ print_t keys[] = {
 {"d", numbers},
 {"%", porcentaje},
 {NULL, NULL}, };
-while (keys[compare].type)
+while (keys[compare].type) /* Iterate the array of estrucutre */
 {
-	if (*(keys[compare].type) == x)
+	if (*(keys[compare].type) == x) /* compare the recived specifier with the stored in the structure, if matches call the function stored in member f of the structure */
 	{
 		return (keys[compare].f(_print)); }
 		compare++; }
-if (!keys[compare].type)
+if (!keys[compare].type) /* if a specifier character is not found, print the character % and the character recived */
 {
 	write(1, "%", 1);
 	write(1, &x, 1);
-	return (2); }
+	return (2);/*return the the length of character % and char x recived*/ }
 return (0); }
